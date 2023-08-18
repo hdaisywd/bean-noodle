cebf1e493b913c3192f56d52ca102d2a54a9d44c
Taeshiki <durudurutta@gmail.com>
2023-08-18 17:18:02 +0900

    태영 브랜치 새로운 프로젝트에 옮기기

--- BeanNoddle/HomeScreen.swift
+++ BeanNoddle/HomeScreen.swift
@@ -2,63 +2,63 @@ import UIKit
 
 class HomeScreen: UIViewController {
 
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .white
-        return cv
-    }()
-    
-    var Search: [Feed] = [] // Replace 'Feed' with your actual movie data structure
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureCollectionView()
-        loadMovies()
-    }
+//    let collectionView: UICollectionView = {
+//        let layout = UICollectionViewFlowLayout()
+//        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        cv.backgroundColor = .white
+//        return cv
+//    }()
+//
+//    var Search: [Feed] = [] // Replace 'Feed' with your actual movie data structure
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        configureCollectionView()
+//        loadMovies()
+//    }
 }
 
 // MARK: - Collection View Configuration
-extension HomeScreen {
-    func configureCollectionView() {
-        collectionView.register(MainCell.self, forCellWithReuseIdentifier: "MainCell")
-        view.addSubview(collectionView)
-        collectionView.frame = view.bounds
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-}
-
-// MARK: - Collection View Delegate & Data Source
-extension HomeScreen: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: 150)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return Search.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCell", for: indexPath) as? MainCell else {
-            return UICollectionViewCell()
-        }
-        
-        let countFeed = Search[indexPath.item]
-        cell.configure(with: countFeed)
-        
-        return cell
-    }
-}
-
-// MARK: - Data Loading
-extension HomeScreen {
-    func loadMovies() {
-        // Load your movie data and populate the 'movies' array
-        // movies = ...
-        
-        collectionView.reloadData()
-    }
-}
+//extension HomeScreen {
+//    func configureCollectionView() {
+//        collectionView.register(MainCell.self, forCellWithReuseIdentifier: "MainCell")
+//        view.addSubview(collectionView)
+//        collectionView.frame = view.bounds
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+//    }
+//}
+//
+//// MARK: - Collection View Delegate & Data Source
+//extension HomeScreen: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 100, height: 150)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return Search.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCell", for: indexPath) as? MainCell else {
+//            return UICollectionViewCell()
+//        }
+//
+//        let countFeed = Search[indexPath.item]
+//        cell.configure(with: countFeed)
+//
+//        return cell
+//    }
+//}
+//
+//// MARK: - Data Loading
+//extension HomeScreen {
+//    func loadMovies() {
+//        // Load your movie data and populate the 'movies' array
+//        // movies = ...
+//
+//        collectionView.reloadData()
+//    }
+//}
 
 // ... Add more extensions or methods as needed ...
--- BeanNoddle/MyPageScreen.swift
+++ BeanNoddle/MyPageScreen.swift
@@ -8,6 +8,53 @@
 import Foundation
 import UIKit
 
-class MyPageScreen: UIViewController {
+class MyPageScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var PostCount: UILabel!
+    @IBOutlet weak var FollowerCount: UILabel!
+    @IBOutlet weak var FollowingCount: UILabel!
+    
+    @IBAction func profileEditButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func profileShare(_ sender: UIButton) {
+    }
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let spacing: CGFloat = 10
+            let itemWidth = (collectionView.frame.width - spacing) / 2
+            layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+            layout.minimumInteritemSpacing = spacing
+            layout.minimumLineSpacing = spacing
+        }
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileImage.clipsToBounds = true
+    }
     
 }
+
