600e8ec5d290012dd143b76081d133cdbfeefab7
Taeshiki <durudurutta@gmail.com>
2023-08-20 17:39:43 +0900

    새 브랜치 생성

--- BeanNoddle/MyPageScreen.swift
+++ BeanNoddle/MyPageScreen.swift
@@ -8,6 +8,41 @@
 import Foundation
 import UIKit
 
-class MyPageScreen: UIViewController {
+class MyPageScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var PostCount: UILabel!
+    @IBOutlet weak var FollowerCount: UILabel!
+    @IBOutlet weak var FollowingCount: UILabel!
+    
+    @IBOutlet weak var highlightCollectionView: UICollectionView!
+    
+    
+    func collectionView(_ highlightCollectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+        
+    }
+    //다시 만들어야됨
+    func collectionView(_ highlightCollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = highlightCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        highlightCollectionView.dataSource = self
+        highlightCollectionView.delegate = self
+        
+        
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileImage.clipsToBounds = true
+    }
     
 }
+
+
